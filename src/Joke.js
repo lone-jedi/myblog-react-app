18e08362e2680ca9f96b3cd4a015e7f3d9ec27cc
Alexander Yarkin <alexander.yarkin33@gmail.com>
2022-07-10 17:27:17 +0300

    initial commit

---
+++ src/Joke.js
@@ -0,0 +1,25 @@
+
+import React, {Component} from 'react';
+
+
+export default class Joke extends Component {
+    state = {};
+
+    componentDidMount() {
+        this.dadJokes()
+    }
+
+    dadJokes = () => {
+        fetch('/api/v1/jokes/one')
+            .then(response => response.json())
+            .then(message => {
+                this.setState({message: message[0]});
+            });
+    };
+
+    render() {
+        return (
+            <h3 className="">{this.state.message}</h3>
+    );
+    }
+}
