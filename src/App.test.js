bc1a8a2a3c9c0c4054c34536344d5f593403438a
Alexander Yarkin <alexander.yarkin33@gmail.com>
2022-07-11 20:02:03 +0300

    Fix test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const pTag = screen.getByText(/Here is your joke :/i);
+  expect(pTag).toBeInTheDocument();
 });
